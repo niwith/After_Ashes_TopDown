933249fe031de05e042e97c0ed2f6d5a958e78b6
niwith <niwith@gmail.com>
2014-07-26 16:51:06 +1000

    Added zoom

---
+++ After_Ashes_Gamefiles/Assets/Scripts/Camera/Camera.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+//zoom variables
+var zoom: float = 10;
+var zoommin: float = 1;
+var zoommax: float = 100;
+
+function Start () {
+
+}
+
+function Update () {
+	camerazoomish();
+}
+
+function camerazoomish () {
+	zoom -= Input.GetAxis("Mouse ScrollWheel") * 10;	
+	if(zoom < zoommin)
+	{
+		zoom = zoommin;
+	} 
+	
+	if(zoom > zoommax)
+	{
+		zoom = zoommax;
+	}
+	camera.orthographicSize = zoom;
+}
